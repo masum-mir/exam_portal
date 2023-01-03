d5657b70c32348257646085c6ccdb5738f1e4a51
masum <masummir.lm@gmail.com>
2023-01-03 22:47:58 +0600

    project updated

---
+++ exam_portal_backend/src/main/java/com/exam/exam_portal_backend/ExamPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.exam.exam_portal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPortalBackendApplication.class, args);
+	}
+
+}
